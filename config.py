4d7e4655339bb7de1cbd7b705175a98d8cddca05
m.grinin <m.grinin@vkteam.ru>
2025-03-15 12:36:20 +0300

    rm prev files

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-FATSECRET_KEY = os.getenv('FATSECRET_KEY')
-FATSECRET_SECRET = os.getenv('FATSECRET_SECRET')
-
--- middlewares.py
+++
@@ -1,8 +0,0 @@
-from aiogram import BaseMiddleware
-from aiogram.types import Message
-
-
-class LoggingMiddleware(BaseMiddleware):
-    async def __call__(self, handler, message: Message, data: dict):
-        print(f'Получено сообщение: {message.text}')
-        return await handler(message, data)
